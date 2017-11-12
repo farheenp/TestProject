d63c2648429be43bc746eb6776911d9dc94f7fa0
farheenp <farheenp@users.noreply.github.com>
2017-11-12 16:36:27 -0600

    Add files via upload

--- Angular2MVC/app/Components/home.component.ts
+++ Angular2MVC/app/Components/home.component.ts
@@ -1,8 +1,94 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, OnInit } from "@angular/core";
+import { UserService } from '../Service/user.service';
+import { Global } from '../Shared/global';
+import { IUser } from '../Model/user';
+
+
+
 
 @Component({
-    template: `<img src="../../images/users.png" style="text-align:center"/>`
+    template: `<img src="../../images/users.png" style="text-align:center"/>
+<br/>
+<div name="userNameDiv">
+ <label for="username">Username</label>
+ <input #username type="text" name="username" required />
+</div>
+
+<div name="passwordDiv">
+ <label for="password">Password</label>
+<input #password type="password" name="password" required/>
+</div>
+
+<div name="buttonDiv">
+ <button (click)="login(username.value,password.value)" class="btn btn-primary">Login</button>
+<button (click)="logout()" class="btn btn-primary">Logout</button>
+</div>
+
+
+
+<br/>
+    {{clickMessage}}
+`
+
 })
 
-export class HomeComponent{
+export class HomeComponent implements OnInit{
+    //model: any = {};
+    clickMessage = '';
+    users: IUser[];
+    user: IUser;
+    msg: string;
+    indLoading: boolean = false;
+    constructor(private _userService: UserService) { }
+
+
+    ngOnInit(): void {
+        this.LoadUsers();
+    }
+
+    logout()
+    {
+        sessionStorage.removeItem("currentUser");
+        this.clickMessage = "logout Successfully";
+    }
+    login(username: string, password: string) {
+  var usernameIsfound= false;
+  var passwordIsNotFound = true;
+  var currentUser = JSON.parse(sessionStorage.getItem("CurrentUser"));
+  if (currentUser != null && currentUser.LastName == username)
+  {
+      this.clickMessage = 'Login Successfully !';
+      return;
+  }
+     for (var i = 0; i < this.users.length; i++)
+     {
+         if (this.users[i].LastName == username)
+         {
+             usernameIsfound = true;
+             // we found the user, match firstname 
+             if (password == this.users[i].FirstName)
+             {
+                 this.clickMessage = 'Login Successfully !';
+                 sessionStorage.setItem("currentUser", JSON.stringify(this.users[i]));
+                 return;
+             }
+             else {
+                 passwordIsNotFound = true;
+             }
+         }
+     }
+     this.clickMessage = 'Login failed !';
+     if (usernameIsfound)
+         this.clickMessage = this.clickMessage + "Due to not matching password!";
+     else
+     this.clickMessage = this.clickMessage + "username  is not found";
+    }
+
+ LoadUsers(): void {
+     this.indLoading = true;
+     this._userService.get(Global.BASE_USER_ENDPOINT)
+         .subscribe(users => { this.users = users; this.indLoading = false; },
+         error => this.msg = <any>error);
+ }
+
 }
--- Angular2MVC/app/Components/user.component.ts
+++ Angular2MVC/app/Components/user.component.ts
@@ -3,6 +3,9 @@ import { UserService } from '../Service/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { IUser } from '../Model/user';
+import { userItem } from '../Model/UserItem';
+import { shoppingCart } from '../Model/shoppingCart';
+
 import { DBOperation } from '../Shared/enum';
 import { Observable } from 'rxjs/Rx';
 import { Global } from '../Shared/global';
@@ -15,6 +18,7 @@ export class UserComponent implements OnInit {
 
     @ViewChild('modal') modal: ModalComponent;
     users: IUser[];
+    selectedUsers: IUser[]= [];
     user: IUser;
     msg: string;
     indLoading: boolean = false;
@@ -22,6 +26,9 @@ export class UserComponent implements OnInit {
     dbops: DBOperation;
     modalTitle: string;
     modalBtnTitle: string;
+    listFilter: string;
+    searchTitle: string = "Search User";
+    currentShoppingCart: shoppingCart;
 
     constructor(private fb: FormBuilder, private _userService: UserService) { }
 
@@ -33,8 +40,49 @@ export class UserComponent implements OnInit {
             Gender: ['', Validators.required]
         });
         this.LoadUsers();
+        this.LoadShoppingCart();
     }
 
+    checkboxChanged(userIdString: string)
+
+    {
+        var userId = parseInt(userIdString);
+        for (var i = 0; i < this.selectedUsers.length; i++)
+        { // delete selected userlist
+            if (this.selectedUsers[i].Id == userId)
+            {
+                this.selectedUsers.splice(i, 1);
+                return;
+            }
+            
+
+        }
+        //  if users is not in selected userlist
+       
+    var currentSelectedUser = this.findUserById(userId);
+    if (currentSelectedUser != null) {
+        this.selectedUsers.push(currentSelectedUser);
+    }
+
+    }
+
+    findUserById(userId: number)
+    {
+        for (var i = 0; i < this.users.length; i++)
+        {
+            if (this.users[i].Id == userId)
+            {
+                return this.users[i];
+            }
+        }
+        return null;
+    }
+
+    LoadShoppingCart()
+    {
+
+        var selectedItemList = this.convertUserListToUserItemList(this.selectedUsers);
+    }
     LoadUsers(): void {
         this.indLoading = true;
         this._userService.get(Global.BASE_USER_ENDPOINT)
@@ -49,6 +97,70 @@ export class UserComponent implements OnInit {
         this.modalBtnTitle = "Add";
         this.userFrm.reset();
         this.modal.open();
+    }
+    purchaseItems()
+    {
+        // current users is already login, we retrive currentUserId first
+        var currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+        var selectedItemList = this.convertUserListToUserItemList(this.selectedUsers);
+        // check if this user has a shopping cart already?
+        this.currentShoppingCart = JSON.parse(localStorage.getItem("currentShoppingcart" + currentUser.Id));
+        if (this.currentShoppingCart == null)
+        {
+            this.currentShoppingCart = new shoppingCart();
+            this.currentShoppingCart.userId = currentUser.Id;
+            this.currentShoppingCart.userItemList = selectedItemList;
+        }
+        else 
+        {
+            this.currentShoppingCart.userItemList = this.mergeItemList(this.currentShoppingCart.userItemList, selectedItemList);
+            //this.currentShoppingCart
+        }
+        localStorage.setItem("currentShoppingcart" + currentUser.Id, JSON.stringify(this.currentShoppingCart));
+
+    }
+
+    convertUserListToUserItemList(selectesUsers: IUser[])
+    {
+        var userItemlist: userItem[]= [];
+
+        for (var i = 0; i < selectesUsers.length; i++)
+        {
+            var tempUserItem = new userItem();
+            tempUserItem.Quantity = 1;
+            tempUserItem.user = selectesUsers[i];
+            tempUserItem.subTotal = 10.00 * tempUserItem.Quantity;
+            userItemlist.push(tempUserItem);
+        }
+        return userItemlist;
+    }
+    mergeItemList(existingItems: userItem[], newItemList: userItem[])
+    {
+        var matchingFlag = false;
+        for (var i = 0; i < existingItems.length; i++)
+        {
+            var currentItem = existingItems[i];
+            matchingFlag = false;
+            for (var j = 0; j < newItemList.length; j++)
+            {
+                if (currentItem.user.Id == newItemList[j].user.Id)
+                {
+                    matchingFlag = true;
+                    // new items are duplicated with existing Item
+                    newItemList[j].Quantity = currentItem.Quantity + newItemList[j].Quantity;
+                    break;
+
+                }
+                
+            }
+            // end  of the loop, find existing item index=i; does not match any newItems
+
+            if (!matchingFlag)
+            {
+                newItemList.push(currentItem);
+            }
+        }
+        return newItemList;
     }
 
     editUser(id: number) {
@@ -140,5 +252,9 @@ export class UserComponent implements OnInit {
     SetControlsState(isEnable: boolean)
     {
         isEnable ? this.userFrm.enable() : this.userFrm.disable();
+    }
+    criteriaChange(value: string): void {
+        if (value != '[object Event]')
+            this.listFilter = value;
     }
 }
